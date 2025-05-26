38f81c5f0b3d7a7637248b51a7e9ff5025191c76
syotakokichi <syota.yamanaka@scogr.co.jp>
2025-05-26 18:43:52 +0900

    Add public folder and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
 }
+
+module.exports = nextConfig
